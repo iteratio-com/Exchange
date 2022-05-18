3dc1479eac7fdc175e554bce85c20ca524bc2be8
Mathias Götze <Mathias.Goetze@iteratio.com>
2022-05-18 13:38:37 +0200

    removed spaces in cmd

--- SAP-MaxDB/share/check_mk/agents/plugins/maxdb.py
+++ SAP-MaxDB/share/check_mk/agents/plugins/maxdb.py
@@ -42,15 +42,16 @@ def parse_config_file():
 def main():
     config = parse_config_file()
     known_querys = {
-        'state': ['db_state'],
+        'state': [['db_state']],
         'backup:sep(124)':
-        ['backup_history_list -c LABEL,ACTION,START,STOP,RC,ERROR -I'],
-        'data:sep(61)': [
-            "sql_executenice SELECT A.USEDSIZE,A.USABLESIZE,S.USED_LOG_PAGES*D.PAGESIZE_IN_KB,"
+        [['backup_history_list', '-c', 'LABEL,ACTION,START,STOP,RC,ERROR', '-I']],
+        'data:sep(61)': [[
+            "sql_executenice", "SELECT", "A.USEDSIZE,A.USABLESIZE,S.USED_LOG_PAGES*D.PAGESIZE_IN_KB,"
             "S.USED_LOG_PAGES,D.ACTIVE_SESSIONS,D.DATACACHE_HITRATE,S.LOG_PAGES*D.PAGESIZE_IN_KB,"
             "S.LOG_PAGES,S.LOG_NOT_SAVED,D.MAXUSERS,D.DATABASEFULL,D.AUTOSAVESTANDBY,D.BADINDEXES,"
-            "D.LOGFULL,D.BADVOLUMES FROM SYSDD.DATASTATISTICS A,SYSDD.SERVERDB_STATS S,SYSDD.DBM_STATE D"
-        ],
+            "D.LOGFULL,D.BADVOLUMES", "FROM", "SYSDD.DATASTATISTICS", "A,SYSDD.SERVERDB_STATS",
+            "S,SYSDD.DBM_STATE", "D"
+        ]],
     }
     for key, value in config.items():
         run_checks = eval(value.get('modules', "['state']"))
@@ -59,8 +60,8 @@ def main():
             print(f"Stop Plugin, hence {cmd} does not match Regex")
             sys.exit(2)
         cmd_line = [
-            cmd, f'-d {key}',
-            f"-u {value.get('user')},{value.get('password')}"
+            cmd, '-d', key, '-u',
+            f"{value.get('user')},{value.get('password')}"
         ]
         for check, queries in known_querys.items():
             if check in run_checks:
@@ -69,14 +70,14 @@ def main():
                 for query in queries:
                     print(
                         run_query(cmd=cmd_line,
-                                  query=[query],
+                                  query=query,
                                   dbname=key,
                                   query_timeout=int(value.get('timeout',
                                                               '20'))))
                 if check == 'state':
                     print(
                         run_query(cmd=["/sapdb/programs/bin/sdbregview", "-l"],
-                                  query="",
+                                  query=None,
                                   dbname=key,
                                   query_timeout=int(value.get('timeout',
                                                               '20'))))
