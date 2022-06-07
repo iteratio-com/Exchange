72141a78d59d7430e5ea53ce46d49e368172d029
Mathias Götze <Mathias.Goetze@iteratio.com>
2022-06-07 15:01:36 +0200

    fix for Backup, when no jobs run and when there are erros

--- SAP-MaxDB/lib/python3/cmk/base/plugins/agent_based/maxdb_backup.py
+++ SAP-MaxDB/lib/python3/cmk/base/plugins/agent_based/maxdb_backup.py
@@ -23,7 +23,8 @@ def parse_maxdb_backup(string_table):
                 begin = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
                 end = datetime.strptime(stop, "%Y-%m-%d %H:%M:%S")
                 duration = (end - begin).total_seconds()
-            age = current_time - int((begin - datetime(1970, 1, 1)).total_seconds())
+            age = current_time - int(
+                (begin - datetime(1970, 1, 1)).total_seconds())
             parsed[srv].setdefault(job, {})
             parsed[srv][job] = {
                 'start': start,
@@ -37,12 +38,14 @@ def parse_maxdb_backup(string_table):
             if result.strip() != '0' or error_msg.strip():
                 parsed[srv].setdefault('error_jobs', []).append(job)
             if job.startswith('DAT'):
-                if parsed[srv].get('last_data') and parsed[srv]['last_data'][0] < age:
+                if parsed[srv].get(
+                        'last_data') and parsed[srv]['last_data'][0] < age:
                     parsed[srv]['last_data'] = [age, job]
                 elif not parsed[srv].get('last_data'):
                     parsed[srv]['last_data'] = [age, job]
             elif job.startswith('LOG'):
-                if parsed[srv].get('last_log') and parsed[srv]['last_log'][0] < age:
+                if parsed[srv].get(
+                        'last_log') and parsed[srv]['last_log'][0] < age:
                     parsed[srv]['last_log'] = [age, job]
                 elif not parsed[srv].get('last_log'):
                     parsed[srv]['last_log'] = [age, job]
@@ -70,32 +73,43 @@ def check_maxdb_backup(item, params, section):
     if not data.get('error_jobs'):
         yield Result(state=State.OK, summary="No Backup job errors")
     else:
-        txt = ""
-        for item in error_jobs:
-            job_infos = data.get(item)
-            txt += f"{item} - started: {job_infos.get('start')} - duration: {render.timespan(job_infos.get('duration'))}"
-            txt += f" - Type: {job_infos.get('type')} - Age : {render.timespan(job_infos.get('age'))}"
-            txt += f" - MSG: {job_infos.get('error_msg')}\n"
-        yield Result(state=State.WARN,
-                     summary=f"There are {len(data.get('error_jobs'))} (see long output)",
-                     details=txt)
+        txt = []
+        for job in data['error_jobs']:
+            job_infos = data.get(job)
+            txt += [
+                f"{job} - started: {job_infos.get('start')} - duration: {render.timespan(job_infos.get('duration'))}"
+                f" - Type: {job_infos.get('type')} - Age: {render.timespan(job_infos.get('age'))}"
+                f" - MSG: {job_infos.get('error_msg')}"
+            ]
+        yield Result(
+            state=State.WARN,
+            summary=
+            f"There are {len(data.get('error_jobs'))} Jobs with Error (see long output)",
+            details='\n'.join(txt))
 
     last_data, last_data_name = data.get('last_data', (None, None))
     last_log, last_log_name = data.get('last_log', (None, None))
-    yield from check_levels(
-        last_data,
-        levels_upper=params.get('age_data'),
-        metric_name="last_data_job",
-        label=f"Last Data Backup Job ({last_data_name})",
-        render_func=render.timespan,
-    )
-    yield from check_levels(
-        last_log,
-        levels_upper=params.get('age_log'),
-        metric_name="last_log_job",
-        label=f"Last Log Backup Job ({last_log_name})",
-        render_func=render.timespan,
-    )
+    if last_data:
+        yield from check_levels(
+            last_data,
+            levels_upper=params.get('age_data'),
+            metric_name="last_data_job",
+            label=f"Last Data Backup Job ({last_data_name})",
+            render_func=render.timespan,
+        )
+    else:
+        yield Result(state=State.OK, summary="No data jobs found")
+
+    if last_log:
+        yield from check_levels(
+            last_log,
+            levels_upper=params.get('age_log'),
+            metric_name="last_log_job",
+            label=f"Last Log Backup Job ({last_log_name})",
+            render_func=render.timespan,
+        )
+    else:
+        yield Result(state=State.OK, summary="No log jobs found")
 
 
 register.check_plugin(
